d527c530922c9175e53317592409ce1633082d75
geromt <gerardomartinez205@gmail.com>
2024-11-30 18:45:53 -0600

    feat: Cambia colores de LinearChart y agrega de lightmode y darkmode

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import './App.css'
 import siiaLogo from './assets/siia_sm.png'
 import githubLogo from './assets/github.svg'
 import facMedicina from './assets/facmedicina.jpg'
+import sunSVG from './assets/sun.svg'
+import moonSVG from './assets/moon.svg'
 import { PatentsTable } from './components/PatentsTable';
 import { useParallax } from 'react-scroll-parallax';
 
@@ -15,6 +17,8 @@ function App() {
   const [isbn, setIsbn] = useState(null)
   const [projects, setProjects] = useState(null)
   const [patents, setPatents] = useState(null)
+  const [mode, setMode] = useState('light')
+  const [modeIcon, setModeIcon] = useState(sunSVG)
   const parallax = useParallax({
     speed: -25,
     opacity: [0.2, 0]
@@ -34,21 +38,36 @@ function App() {
     refreshPatents();
   }, []);
 
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      setModeIcon(moonSVG);
+    } else {
+      setMode('light');
+      setModeIcon(sunSVG);
+    }
+  }
+
+  const colors = ['#AF2BBF', "#51CB20", "#E4572E", "#773344", "#DA3E52"]
+
   return (
-    <>
+    <div className={mode}>
       <header className='w-full h-24 flex flex-row bg-transparent align-middle items-center justify-around absolute z-50'>
         <a href="https://web.siia.unam.mx/siia-publico/index.php" target="_blank">
           <img src={siiaLogo} className="logo basis-1/4" alt="SIIA logo" />
         </a>
         <h2 className='text-center text-2xl basis-auto font-mono text-dark-secondary'>Sistema Integral de Información Académica</h2>
-        <div>
-          <a href="https://github.com/geromt/informe-prueba" target="_blank" className='h-24 w-24 basis-1/6'>
+        <div className='basis-1/6 flex flex-row'>
+          <a href="https://github.com/geromt/informe-prueba" target="_blank" className='h-24 w-24'>
             <img src={githubLogo} className='w-24 h-24 p-6 hover:scale-105' alt="Github logo" />
           </a>
+          <button className='h-24 w-24 bg-transparent hover:border-0' onClick={toggleMode}>
+            <img src={modeIcon} className='w-full h-full hover:scale-105' alt="Mode switcher" />
+          </button>
         </div>
       </header>
       <main>
-        <div className="relative snap-start h-screen flex flex-row items-center justify-between">
+        <div className="relative h-screen flex flex-row items-center justify-between">
           <img ref={parallax.ref} src={facMedicina} className="absolute w-full h-full object-cover" alt="Facultad de Medicina" />
           <h1 className='basis-full text-dark-primary font-bold text-7xl'>Facultad de Medicina</h1>
           <div className='flex flex-col basis-80 h-2/3 justify-center z-10 right-0 flex-initial'>
@@ -70,16 +89,15 @@ function App() {
             hover:text-dark-primary hover:bg-dark-primary/5 hover:scale-x-105 duration-300'>Personal Académico</a>
           </div>
         </div>
+        
         <div className='h-96 bg-[#242424] dark:bg-dark-background'></div>
-        <div className='h-screen w-full mx:auto overflow-y-scroll flex flex-col snap-y snap-mandatory container'>
-          {documents && <LinearChart title="Documentos" data={documents} />}
-          {articles && <LinearChart title="Articulos" data={articles} />}
-          {isbn && <LinearChart title="ISBN" data={isbn} />}
-          {projects && <BarLineChart title="Proyectos" data={projects} />}
-          {patents && <PatentsTable title="Patentes" data={patents} />}
-        </div>
+        {documents && <LinearChart title="Documentos" data={documents} colors={colors}/>}
+        {articles && <LinearChart title="Articulos" data={articles} colors={colors}/>}
+        {isbn && <LinearChart title="ISBN" data={isbn} colors={colors}/>}
+        {projects && <BarLineChart title="Proyectos" data={projects} />}
+        {patents && <PatentsTable title="Patentes" data={patents} />}
       </main>
-    </>
+    </div>
   )
 }
 
--- src/components/LinearChart.jsx
+++ src/components/LinearChart.jsx
@@ -1,17 +1,48 @@
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from "recharts"
+import { AreaChart, Area, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from "recharts"
 import { range } from "../services/fetchServices"
 import { useEffect, useState } from "react";
 import { Button, Dropdown } from "flowbite-react"
 import { PropTypes } from "prop-types"
 
-export function LinearChart({title, data}){
+
+function CustomTooltip({ payload, label, active, data }) {
+  CustomTooltip.propTypes = {
+    payload: PropTypes.arrayOf(PropTypes.object),
+    label: PropTypes.string,
+    active: PropTypes.bool,
+    data: PropTypes.object,
+  }
+  const colors = ['text-[#AF2BBF]', "text-[#51CB20]", "text-[#E4572E]", "text-[#773344]", "text-[#DA3E52]"]
+
+  if (active) {
+    return (
+      <div className="bg-white-background/30 rounded-sm flex flex-col items-center">
+        <p className="text-white-text font-bold text-2xl">{label}</p>
+        {
+          data.keys.map((key, index) => {
+            return <p key={key} className={colors[index]}>{`${key}: ${payload[index].value}`}</p>
+          })
+        }
+      </div>
+    );
+  }
+
+  return null;
+}
+
+
+export function LinearChart({title, data, colors}){
     LinearChart.propTypes = {
         title: PropTypes.string.isRequired,
-        data: PropTypes.object.isRequired
+        data: PropTypes.object.isRequired,
+        colors: PropTypes.array.isRequired
     }
     const [showKeys, setShowKeys] = useState({})
     const [dropdownFrom, setDropdownFrom] = useState(data.to);
     const [dropdownTo, setDropdownTo] = useState(data.from);
+    const [desdeLabel, setDesdeLabel] = useState(`Desde: ${data.from}`)
+    const [hastaLabel, setHastaLabel] = useState(`Hasta: ${data.to}`)
+    const [sexoLabel, setSexoLabel] = useState(`Sexo: Ambos`)
 
     const handleShowKeys = (key) => {
         const newKeys = {...showKeys};
@@ -27,44 +58,66 @@ export function LinearChart({title, data}){
         setShowKeys(initShowKeys)
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+      
 
     return (
-        <div className="bg-dark-background flex flex-col align-middle justify-center h-screen snap-center shrink-0">
-            <h1>{title}</h1>
-            <div>
-                <Dropdown label="Desde">
+        <div className="bg-white-background w-full dark:bg-dark-background flex flex-col items-center justify-center h-screen">
+            <h1 className="text-white-text dark:bg-dark-secondary my-8">{title}</h1>
+            <div className="flex flex-row justify-around w-full my-4">
+                <div className="flex flex-row gap-4 basis-1/2">
+                <Dropdown label={desdeLabel}>
                     {
                         range(dropdownFrom-data.from + 1, data.from).map((i) => {
-                           return <Dropdown.Item onClick={() => setDropdownTo(i)} key={i}>{i}</Dropdown.Item>;
+                           return <Dropdown.Item className="bg-cyan-800 text-white border-cyan-500  hover:text-black" onClick={() => {
+                            setDropdownTo(i);
+                            setDesdeLabel(`Desde: ${i}`)}} key={i}>{i}</Dropdown.Item>;
                         })
                     }
                 </Dropdown>
-                <Dropdown label="Hasta">
+                <Dropdown label={hastaLabel}>
                     {
-                        range(data.to-dropdownTo + 1, data.from).map((i) => {
-                           return <Dropdown.Item onClick={() => setDropdownFrom(i)} key={i}>{i}</Dropdown.Item>;
+                        range(data.to-dropdownTo + 1, dropdownTo).map((i) => {
+                           return <Dropdown.Item className="bg-cyan-800 text-white border-cyan500 hover:text-black" onClick={() => {
+                            setDropdownFrom(i)
+                            setHastaLabel(`Hasta: ${i}`)}} key={i}>{i}</Dropdown.Item>;
                         })
                     }
                 </Dropdown>
-                <Dropdown label="Sexo">
-                    <Dropdown.Item>Ambos</Dropdown.Item>
-                    <Dropdown.Item>Masculino</Dropdown.Item>
-                    <Dropdown.Item>Femenino</Dropdown.Item>
+                </div>
+                
+                <Dropdown label={sexoLabel}>
+                    <Dropdown.Item className="bg-cyan-800 text-white border-cyan500 hover:text-black" onClick={() => {setSexoLabel(`Sexo: Ambos`)}}>Ambos</Dropdown.Item>
+                    <Dropdown.Item className="bg-cyan-800 text-white border-cyan500 hover:text-black" onClick={() => {setSexoLabel(`Sexo: Masculino`)}}>Masculino</Dropdown.Item>
+                    <Dropdown.Item className="bg-cyan-800 text-white border-cyan500 hover:text-black" onClick={() => {setSexoLabel(`Sexo: Femenino`)}}>Femenino</Dropdown.Item>
                 </Dropdown>
             </div>
-            <LineChart width={600} height={400} data={data.data}  margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-                {
-                    data.keys.map(key => {
-                        return <Line type="monotone" dataKey={key} stroke="#8884d8" key={key} hide={!showKeys[key]}/>
+            <ResponsiveContainer aspect={2.25} width={1200}>
+              <AreaChart height={400} data={data.data}  margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                <defs>
+                  {
+                    data.keys.map((key, index) => {
+                      return (
+                        <linearGradient id={`color:${index}`} key={key} x1="0" y1="0" x2="0" y2="1">
+                          <stop offset="5%" stopColor={colors[index]} stopOpacity={0.8}/>
+                          <stop offset="95%" stopColor={colors[index]} stopOpacity={0}/>
+                        </linearGradient>
+                      )
                     })
-                }
-                <CartesianGrid stroke="#ccc" strokeDasharray="5 5"/>
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-            </LineChart>
-            <div>
+                  }
+                </defs>
+                  {
+                      data.keys.map((key, index) => {
+                          return <Area type="monotone" dataKey={key} stroke={colors[index]} key={key} fillOpacity={1} fill={`url(#color:${index})`} hide={!showKeys[key]}/>
+                      })
+                  }
+                  <CartesianGrid stroke="#ccc" strokeDasharray="3 3"/>
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Tooltip content={<CustomTooltip data={data}/>}/>
+                  <Legend />
+              </AreaChart>
+            </ResponsiveContainer>
+            <div className="flex flex-row justify-around w-1/2">
                 {
                     data.keys.map(key => {
                         return <Button key={key} onClick={() => handleShowKeys(key)}>
