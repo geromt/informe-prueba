2b7932dba94b9d56411434ed43e7764e1ffe956e
geromt <gerardomartinez205@gmail.com>
2024-12-01 17:24:31 -0600

    feat: Agrega función para filtrar datos por titulo o inventores

--- src/components/PatentsTable.jsx
+++ src/components/PatentsTable.jsx
@@ -1,36 +1,67 @@
-import { Table } from "flowbite-react";
+import { FloatingLabel, Table, Button } from "flowbite-react";
 import { PropTypes } from "prop-types";
+import { useEffect, useRef, useState } from "react";
 
 export function PatentsTable({title, data}){
     PatentsTable.propTypes = {
         title: PropTypes.string.isRequired,
         data: PropTypes.object.isRequired
     }
+    const [dataToTable, setDataToTable] = useState(data.data)
+    const tituloInput = useRef(null);
+    const inventoresInput = useRef(null);
+
+    const filterData = () => {
+      const titulo = tituloInput.current.value
+      const inventores = inventoresInput.current.value
+
+      if (titulo === '' && inventores === '') {
+        setDataToTable(data.data)
+        return
+      }
+      const filteredData = data.data.filter(item => item.Titulo.toUpperCase().includes(titulo.toUpperCase()) && item.Inventores.join(" ").toUpperCase().includes(inventores.toUpperCase()))
+      setDataToTable(filteredData)
+    }
+
+    useEffect(() => {
+      filterData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
     return (
-        <div>
-            <h2>{title}</h2>
-            <Table>
+        <div className="bg-white-background dark:bg-dark-background  flex flex-col items-center justify-center 
+        w-full h-screen snap-center shrink-0">
+          <h1 className="text-white-secondary db-white-background 
+        dark:text-dark-secondary dark:bg-dark-background my-8">{title}</h1>
+          <div className="grid grid-cols-3 gap-4 w-3/4">
+            <FloatingLabel ref={tituloInput} label="Título" variant="filled"/>
+            <FloatingLabel ref={inventoresInput} label="Inventores" variant="filled"/>
+            <Button className="items-center mb-2 mx-24" onClick={() => filterData()}>Buscar</Button>
+          </div>
+            <div className="w-full overflow-x-hidden overflow-y-scroll">
+            <Table striped hoverable>
                 <Table.Head>
-                    <Table.HeadCell>Titulo</Table.HeadCell>
+                    <Table.HeadCell>Título</Table.HeadCell>
                     <Table.HeadCell>Inventores</Table.HeadCell>
                     <Table.HeadCell>Año</Table.HeadCell>
                     <Table.HeadCell>Seccion</Table.HeadCell>
                 </Table.Head>
                 <Table.Body>
                     {
-                        data.data.map(({Id, Titulo, Inventores, Fecha, Seccion}) => {
+                        dataToTable.map(({Id, Titulo, Inventores, Fecha, Seccion}) => {
                             return(
-                                <Table.Row key={Id}>
-                                    <Table.Cell>{Titulo}</Table.Cell>
-                                    <Table.Cell>{Inventores.join("\n")}</Table.Cell>
-                                    <Table.Cell>{Fecha}</Table.Cell>
-                                    <Table.Cell>{Seccion.join("\n")}</Table.Cell>
+                                <Table.Row className="border-y-white-primary border-2" key={Id}>
+                                    <Table.Cell className="text-white-text dark:text-dark-secondary">{Titulo}</Table.Cell>
+                                    <Table.Cell className="text-white-text dark:text-dark-secondary">{Inventores.join("\n")}</Table.Cell>
+                                    <Table.Cell className="text-white-text dark:text-dark-secondary">{Fecha}</Table.Cell>
+                                    <Table.Cell className="text-white-text dark:text-dark-secondary">{Seccion.join("\n")}</Table.Cell>
                                 </Table.Row>
                             )
                         })
                     }
                 </Table.Body>
             </Table>
+            </div>
         </div>
     )
 }
